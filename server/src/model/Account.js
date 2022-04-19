ed89cf7ca328f604c3c268316436001aa2205de8
unknown <dbvhoang@gmail.com>
2022-04-19 17:00:05 +0700

    fix merge

--- server/src/model/Account.js
+++ server/src/model/Account.js
@@ -1,52 +1,35 @@
-<<<<<<< HEAD
 const { DataTypes } = require("sequelize");
 const sequelize = require("../database/config");
 const User = require("./User");
 const Role = require("./Role");
-const RoleAccounts = require("RoleAccounts");
-
-const Account = sequelize.define(
-  "account",
-  {
-    id: {
-      type: DataTypes.INTEGER,
-      autoIncrement: true,
-      allowNULL: false,
-      primaryKey: true,
-=======
-const { DataTypes } = require('sequelize');
-const sequelize = require('../database/config');
-const User = require('./User');
-const Role = require('./Role');
-const Account = sequelize.define('account', {
-    id: {
-        type: DataTypes.UUID,
-        defaultValue: DataTypes.UUIDV4,
-        allowNULL: false,
-        primaryKey: true,        
->>>>>>> dev-backend
+const Account = sequelize.define("account", {
+  id: {
+    type: DataTypes.UUID,
+    defaultValue: DataTypes.UUIDV4,
+    allowNULL: false,
+    primaryKey: true,
+  },
+  username: {
+    type: DataTypes.STRING,
+    allowNULL: false,
+    unique: true,
+  },
+  password: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  email: {
+    type: DataTypes.STRING,
+    allowNULL: true,
+    unique: true,
+    validate: {
+      isEmail: true,
     },
-    username: {
-      type: DataTypes.STRING,
-      allowNULL: false,
-      unique: true,
-    },
-    password: {
-      type: DataTypes.STRING,
-      allowNull: false,
-    },
-    email: {
-      type: DataTypes.STRING,
-      allowNULL: true,
-      unique: true,
-      validate: {
-        isEmail: true,
-      },
-    },
-    facebook_id: {
-        type: DataTypes.STRING,
-        allowNULL: true,
-    }
+  },
+  facebook_id: {
+    type: DataTypes.STRING,
+    allowNULL: true,
+  },
 });
 
 Account.hasOne(User);
