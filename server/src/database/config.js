2bfe28e86601a9e46a534f250d3ae9d0cf2f3e6e
unknown <dbvhoang@gmail.com>
2022-04-20 17:19:50 +0700

    fix config file

--- server/src/database/config.js
+++ server/src/database/config.js
@@ -1,11 +1,4 @@
-<<<<<<< HEAD
-const { Sequelize } = require('sequelize');
-const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USERNAME, process.env.DB_PASSWORD, {
-    dialect: 'mysql',
-    host: 'localhost',
-});
-=======
-const { Sequelize, Sequelize } = require("sequelize");
+const { Sequelize } = require("sequelize");
 const sequelize = new Sequelize(
   process.env.DB_NAME,
   process.env.DB_USERNAME,
@@ -15,21 +8,14 @@ const sequelize = new Sequelize(
     host: "localhost",
   }
 );
->>>>>>> Hoang_BackEnd
 
 // ----- IMPORTANT -----
 // If force = true, everytime the server runs we will lose the data for the table
 // force = true recreates a table
-<<<<<<< HEAD
-sequelize.sync({ force: false });
-
-(async() => {
-=======
 
 sequelize.sync({ force: false, alter: true });
 
 (async () => {
->>>>>>> Hoang_BackEnd
   try {
     await sequelize.authenticate();
     console.log("database connect successfully");
@@ -38,8 +24,4 @@ sequelize.sync({ force: false, alter: true });
   }
 })();
 
-<<<<<<< HEAD
 module.exports = sequelize;
-=======
-module.exports = sequelize;
->>>>>>> Hoang_BackEnd
