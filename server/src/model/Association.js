ed8f3eefa08d10f30f2dda2636e90de4d30c8687
unknown <dbvhoang@gmail.com>
2022-04-21 21:13:59 +0700

    them 2 chieu association

--- server/src/model/Association.js
+++ server/src/model/Association.js
@@ -13,29 +13,57 @@ const Service = require("./Service");
 const ServiceEvents = require("./ServiceEvents");
 const User = require("./User");
 
+// Belongsto index
+// @User
 User.belongsTo(Account, { foreignKey: "accountId", targetKey: "id" });
+Account.hasOne(User, { foreignKey: "accountId", sourceKey: "id" });
 
-Bill.belongsTo(User, { foreignKey: "UserId", targetKey: "id" });
+// @Bill
+Bill.belongsTo(User, { foreignKey: "userId", targetKey: "id" });
+User.hasMany(Bill, { foreignKey: "userId", sourceKey: "id" });
 
+//@BillDetail
 BillDetail.belongsTo(Bill, { foreignKey: "billId", targetKey: "id" });
 BillDetail.belongsTo(Service, { foreignKey: "serviceId", targetKey: "id" });
+Bill.hasMany(BillDetail, { foreignKey: "billId", sourceKey: "id" });
+Service.hasMany(BillDetail, { foreignKey: "serviceId", sourceKey: "id" });
 
+//@Cart
 Cart.belongsTo(Bill, { foreignKey: "billId", targetKey: "id" });
 Cart.belongsTo(Service, { foreignKey: "serviceId", targetKey: "id" });
+Bill.hasMany(Cart, { foreignKey: "billId", sourceKey: "id" });
+Service.hasMany(Cart, { foreignKey: "serviceId", sourceKey: "id" });
 
+//@Image
 Image.belongsTo(Service, { foreignKey: "serviceId", targetKey: "id" });
+Service.hasMany(Image, { foreignKey: "serviceId", sourceKey: "id" });
 
+//@Rate
 Rate.belongsTo(User, { foreignKey: "userId", targetKey: "id" });
 Rate.belongsTo(Service, { foreignKey: "serviceId", targetKey: "id" });
+User.hasMany(Rate, { foreignKey: "userId", sourceKey: "id" });
+Service.hasMany(Rate, { foreignKey: "serviceId", sourceKey: "id" });
 
+//RoleAccounts
 RoleAccounts.belongsTo(Role, { foreignKey: "roleId", targetKey: "id" });
 RoleAccounts.belongsTo(Account, { foreignKey: "accountId", targetKey: "id" });
+Role.hasMany(RoleAccounts, { foreignKey: "roleId", sourceKey: "id" });
+Account.hasMany(RoleAccounts, { foreignKey: "accountId", sourceKey: "id" });
 
+//@RolePermissions
 RolePermissions.belongsTo(Role, { foreignKey: "roleId", targetKey: "id" });
 RolePermissions.belongsTo(Permission, {
   foreignKey: "permissionId",
   targetKey: "id",
 });
+Role.hasMany(RolePermissions, { foreignKey: "roleId", sourceKey: "id" });
+Permission.hasMany(RolePermissions, {
+  foreignKey: "permissionId",
+  sourceKey: "id",
+});
 
+//@ServiceEvents
 ServiceEvents.belongsTo(Service, { foreignKey: "serviceId", targetKey: "id" });
 ServiceEvents.belongsTo(Event, { foreignKey: "eventId", targetKey: "id" });
+Service.hasMany(ServiceEvents, { foreignKey: "serviceId", sourceKey: "id" });
+Event.hasMany(ServiceEvents, { foreignKey: "eventId", sourceKey: "id" });
