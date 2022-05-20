8838a36b33f77f76aaa1c57edca9adb4be9bf770
hoangdev01 <dbvhoang@gmail.com>
2022-05-21 01:16:39 +0700

    Add login page success

--- client/src/features/auth/login.component.js
+++ client/src/features/auth/login.component.js
@@ -5,51 +5,49 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 export default class Login extends Component {
   render() {
     return (
-      <div className="container">
-        <div id="form-parent">
-          <h1>Sign In</h1>
-          <form>
-            <div className="mb-3">
-              <label>Username</label>
+      <div className="auth-inner">
+        <h3>Sign In</h3>
+        <form>
+          <div className="mb-3">
+            <label>Username</label>
+            <input
+              type="text"
+              className="form-control"
+              placeholder="Enter username"
+            />
+          </div>
+
+          <div className="mb-3">
+            <label>Password</label>
+            <input
+              type="password"
+              className="form-control"
+              placeholder="Enter password"
+            />
+          </div>
+
+          <div className="mb-3">
+            <div className="custom-control custom-checkbox">
+              <label className="custom-control-label" htmlFor="customCheck1">
+                Remember me
+              </label>
               <input
-                type="text"
-                className="form-control"
-                placeholder="Enter username"
+                type="checkbox"
+                className="custom-control-input"
+                id="customCheck1"
               />
             </div>
+          </div>
 
-            <div className="mb-3">
-              <label>Password</label>
-              <input
-                type="password"
-                className="form-control"
-                placeholder="Enter password"
-              />
-            </div>
-
-            <div className="mb-3">
-              <div className="custom-control custom-checkbox">
-                <label className="custom-control-label" htmlFor="customCheck1">
-                  Remember me
-                </label>
-                <input
-                  type="checkbox"
-                  className="custom-control-input"
-                  id="customCheck1"
-                />
-              </div>
-            </div>
-
-            <div className="d-grid">
-              <button type="submit" className="btn btn-primary">
-                Submit
-              </button>
-            </div>
-            <p className="forgot-password text-right">
-              Forgot <a href="#">password?</a>
-            </p>
-          </form>
-        </div>
+          <div className="d-grid">
+            <button type="submit" className="btn btn-primary">
+              Submit
+            </button>
+          </div>
+          <p className="forgot-password text-right">
+            <a href="forget-pasword">Forgot password?</a>
+          </p>
+        </form>
       </div>
     );
   }
--- client/src/features/auth/signup.component.js
+++ client/src/features/auth/signup.component.js
@@ -4,55 +4,65 @@ import './style.css';
 export default class SignUp extends Component {
   render() {
     return (
-      <form>
-        <h3>Sign Up</h3>
+      <div className="auth-inner">
+        <form>
+          <h3>Sign Up</h3>
 
-        <div className="mb-3">
-          <label>First name</label>
-          <input
-            type="text"
-            className="form-control"
-            placeholder="First name"
-          />
-        </div>
+          <div className="mb-3">
+            <label>First name</label>
+            <input
+              type="text"
+              className="form-control"
+              placeholder="First name"
+            />
+          </div>
 
-        <div className="mb-3">
-          <label>Last name</label>
-          <input type="text" className="form-control" placeholder="Last name" />
-        </div>
+          <div className="mb-3">
+            <label>Last name</label>
+            <input
+              type="text"
+              className="form-control"
+              placeholder="Last name"
+            />
+          </div>
 
-        <div className="mb-3">
-          <label>Username</label>
-          <input type="text" className="form-control" placeholder="Username" />
-        </div>
+          <div className="mb-3">
+            <label>Username</label>
+            <input
+              type="text"
+              className="form-control"
+              placeholder="Username"
+            />
+          </div>
 
-        <div className="mb-3">
-          <label>Email address</label>
-          <input
-            type="email"
-            className="form-control"
-            placeholder="Enter email"
-          />
-        </div>
+          <div className="mb-3">
+            <label>Email address</label>
+            <input
+              type="email"
+              className="form-control"
+              placeholder="Enter email"
+            />
+          </div>
 
-        <div className="mb-3">
-          <label>Password</label>
-          <input
-            type="password"
-            className="form-control"
-            placeholder="Enter password"
-          />
-        </div>
+          <div className="mb-3">
+            <label>Password</label>
+            <input
+              type="password"
+              className="form-control"
+              placeholder="Enter password"
+            />
+          </div>
 
-        <div className="d-grid">
-          <button type="submit" className="btn btn-primary">
-            Sign Up
-          </button>
-        </div>
-        <p className="forgot-password text-right">
-          Already registered <a href="/sign-in">sign in?</a>
-        </p>
-      </form>
+          <div className="d-grid">
+            <button type="submit" className="btn btn-primary">
+              Sign Up
+            </button>
+          </div>
+          <p className="forgot-password text-right">
+            Already registered <a href="/sign-in">sign in?</a>
+          </p>
+        </form>
+      </div>
     );
   }
 }
