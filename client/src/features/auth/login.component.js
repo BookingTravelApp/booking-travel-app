1ec4b21e0fc0bc9cd257731a202ecca2245837d7
hoangdev01 <dbvhoang@gmail.com>
2022-05-21 12:48:25 +0700

    Add reducer

--- client/src/features/auth/login.component.js
+++ client/src/features/auth/login.component.js
@@ -1,58 +1,74 @@
-import React, { Component } from 'react';
+import React, { useState, useContext } from 'react';
 import './style.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
-export default class Login extends Component {
-  render() {
-    return (
-      <div className="auth-inner">
-        <h3>Sign In</h3>
-        <form>
-          <div className="mb-3">
-            <label>Username</label>
+function Login(props) {
+  const [loginForm, setLoginForm] = useState({
+    username: '',
+    password: '',
+  });
+  const { username, password } = loginForm;
+  const onchangeLoginForm = event =>
+    setLoginForm({ ...loginForm, [event.target.name]: event.target.value });
+  return (
+    <div className="auth-inner">
+      <h3>Sign In</h3>
+      <form>
+        <div className="mb-3">
+          <label>Username</label>
+          <input
+            type="text"
+            className="form-control"
+            placeholder="Enter username"
+            required
+            name="username"
+            value={username}
+            onChange={onchangeLoginForm}
+          />
+        </div>
+
+        <div className="mb-3">
+          <label>Password</label>
+          <input
+            type="password"
+            className="form-control"
+            placeholder="Enter password"
+            required
+            name="password"
+            value={password}
+            onChange={onchangeLoginForm}
+          />
+        </div>
+
+        <div className="mb-3">
+          <div className="custom-control custom-checkbox">
             <input
-              type="text"
-              className="form-control"
-              placeholder="Enter username"
+              type="checkbox"
+              className="custom-control-input"
+              id="customCheck1"
             />
+            <label className="custom-control-label" htmlFor="customCheck1">
+              Remember me
+            </label>
           </div>
+        </div>
 
-          <div className="mb-3">
-            <label>Password</label>
-            <input
-              type="password"
-              className="form-control"
-              placeholder="Enter password"
-            />
-          </div>
-
-          <div className="mb-3">
-            <div className="custom-control custom-checkbox">
-              <input
-                type="checkbox"
-                className="custom-control-input"
-                id="customCheck1"
-              />
-              <label className="custom-control-label" htmlFor="customCheck1">
-                Remember me
-              </label>
-            </div>
-          </div>
-
-          <div className="d-grid">
-            <button type="submit" className="btn btn-primary">
-              Submit
-            </button>
-          </div>
-          <p className="forgot-password text-right">
-            <a href="forget-pasword">Forgot password?</a>
-          </p>
-          <hr width="100%" align="center" />
-          <p className="forgot-password text-right">
-            Not a member? <a href="sign-up">Sign up now</a>
-          </p>
-        </form>
-      </div>
-    );
-  }
+        <div className="d-grid">
+          <button type="submit" className="btn btn-primary">
+            Submit
+          </button>
+        </div>
+        <p className="forgot-password text-right">
+          <a href="forget-pasword">Forgot password?</a>
+        </p>
+        <br />
+        <hr width="100%" align="center" />
+        <p className="forgot-password text-right">
+          Not a member? <a href="sign-up">Sign up now</a>
+        </p>
+      </form>
+    </div>
+  );
 }
+
+export default Login;
