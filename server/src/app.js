33ddccf89e7332e5d959341b9107925ba25adbf8
hoangdev01 <dbvhoang@gmail.com>
2022-05-21 13:06:25 +0700

    Fix cors server

--- server/src/app.js
+++ server/src/app.js
@@ -18,6 +18,14 @@ const resourceRouter = require("./router/resource");
 
 const app = express();
 const PORT = process.env.PORT || 4000;
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
+});
 app.use(express.json());
 //router
 app.use("/", authRouter);
