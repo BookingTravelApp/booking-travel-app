905ab91e50cc909bf87749784e2fff380a5c6340
hoangdev01 <dbvhoang@gmail.com>
2022-05-23 14:48:56 +0700

    Add logout

--- server/src/router/auth.js
+++ server/src/router/auth.js
@@ -15,4 +15,8 @@ router.post("/register", validator.register(), AuthController.create);
 // @access public
 router.post("/login", AuthController.login);
 
+// @router POST /login
+// @access public
+router.post("/logout", verifyToken, AuthController.logout);
+
 module.exports = router;
