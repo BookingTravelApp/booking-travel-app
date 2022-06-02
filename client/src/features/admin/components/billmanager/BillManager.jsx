72f5fa76093f26d7b468c60faa5669e805f12506
hoangdev01 <dbvhoang@gmail.com>
2022-06-02 13:38:09 +0700

    fix auth

--- client/src/features/admin/components/billmanager/BillManager.jsx
+++ client/src/features/admin/components/billmanager/BillManager.jsx
@@ -1,6 +1,20 @@
 import React from 'react';
+import billApi from '../../../../api/billApi';
+import { useState, useEffect } from 'react';
 
 const BillManager = () => {
+  const [listBill, setListBill] = useState([]);
+  useEffect(() => {
+    billApi
+      .getAll()
+      .then(response => {
+        setListBill(response.data.billList);
+        console.log(response.data);
+      })
+      .catch(error => {
+        console.log('Failed to fetch BillList:', error);
+      });
+  }, []);
   return <div>BillManager</div>;
 };
 
